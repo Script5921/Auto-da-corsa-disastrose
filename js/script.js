1e6f34c1bc939316020e93b0603a98298034c01a
Script5921 <125158487+Script5921@users.noreply.github.com>
2023-03-10 21:48:36 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-main-page","displayName":"Main page","link":{"linkType":"LinkTypePage","href":"#!page-main-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-capitolo-bonus","displayName":"Capitolo bonus","link":{"linkType":"LinkTypePage","href":"#!page-capitolo-bonus"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
